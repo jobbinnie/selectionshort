6b5d725e7fd5d7d01f0db827d59b8738d3d03dc1
jobbinnie <josefasotomayor1124@gmail.com>
2026-06-11 13:56:02 -0400

    Update algoritmo.py

--- algoritmo.py
+++ algoritmo.py
@@ -1,3 +1,8 @@
-lista_1 = [1, 2, 3, 4, 5]
-for i in range(len(lista_1)):
-    print(lista_1[i])
+def selection_sort(lista):
+    for i in range(len(lista)):
+        min_index = i
+        for j in range(i + 1, len(lista)):
+            if lista[j] < lista[min_index]:
+                min_index = j
+        lista[i], lista[min_index] = lista[min_index], lista[i]
+    return lista
