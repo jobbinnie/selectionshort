ffdcb5b9c0f9ecfa02390177c942d5931cbb9406
bkma18 <umo1615@gmail.com>
2026-06-23 20:37:31 -0400

    actualizacion de codigo

--- algoritmo.py
+++ algoritmo.py
@@ -1,16 +1,15 @@
-lista = int(input("Digita el tamaño de la lista: "))
-numeros = []
+lista = input("ingresa lista de números separados por un espacio o comas: ")
+lista_limpia = lista.replace(",", " ").split()
+ingreso = [int(x) for x in lista_limpia]
 
-for _ in range(lista):
-    numero = int(input("Digita un número: "))
-    numeros.append(numero)
-
-def selection_sort(numeros):
-    for i in range(len(numeros)):
+def selection_sort(ingreso):
+    largo= len(ingreso)
+    for i in range(largo - 1):
         min_index = i
-        for j in range(i + 1, len(numeros)):
-            if numeros[j] < numeros[min_index]:
-                min_index = j
-        numeros[i], numeros[min_index] = numeros[min_index], numeros[i]
-    return numeros
-print(selection_sort(numeros))
+
+        for j in range(i + 1, largo):
+            if ingreso[j] < ingreso[min_index]:
+                min_index = j         
+        ingreso[i], ingreso[min_index] = ingreso[min_index], ingreso[i]
+    return ingreso
+print(selection_sort(ingreso))
